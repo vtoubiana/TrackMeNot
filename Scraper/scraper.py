6e94259aa55cdd67908b2883f44872dab593d968
Cameron <cb596@cornell.edu>
2018-05-15 18:26:38 -0400

    cleaning up

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -2,9 +2,10 @@ import codecs
 import re
 import csv
 import datetime
+import pandas as pd
 
 
-def getTrackMeNot():
+def getTrackMeNot(trackMeNotLogFile):
 	'''
 	Crawl through the TrackMeNotLogs.csv - file containing all of the 
 	downloaded trackMeNot logs along with the times at which they were made
@@ -12,8 +13,7 @@ def getTrackMeNot():
 		key: trackMeNotQuery (string)
 		value: array of times at which that trackMeNoT query was made (array of strings)
 	'''
-
-	with open('TrackMeNotLogs.csv','r') as f:
+	with open(trackMeNotLogFile,'r') as f:
 		reader = csv.reader(f)
 		trackMeNotDict = {}
 		for log in reader:
@@ -33,7 +33,8 @@ def parseGoogleTime(googleTime):
 	parses googleTime into a dict so that it can be easily
 	made into a datetime.datetime object
 	example:
-	input googleTime: 'May 13, 2018, 2:48:56 PM'
+		input googleTime= 'May 13, 2018, 2:48:56 PM'
+		output: {'month': 5, 'day' 13,'year': 2018, 'hour': 14, 'minute': 48, 'second', :56}
 	'''
 	googleTimeParsed = {}
 
@@ -88,12 +89,14 @@ def parseTrackMeNotTime(trackMeNotTimes):
 	parses each of the times in trackMeNotTimes 
 	into a dict so that it can be easily made into a 
 	datetime.datetime object
-	example trackMeNotTimes ['22:54:53   5/13/2018', '22:20:19   5/13/2018', '21:55:32   5/13/2018', '21:42:27   5/13/2018']
+	example:
+		input: trackMeNotTimes ['22:54:53   5/13/2018', '22:20:19   5/13/2018', '21:55:32   5/13/2018', '21:42:27   5/13/2018']
+		output: [{'month': 5, 'day' 13,'year': 2018, 'hour': 24, 'minute': 54, 'second', :53}, ...]
 	'''
 	arrayOfParsedDict = []
 	for entry in trackMeNotTimes:
 		parsed_dict = {}
-		print (entry)
+		#print (entry)
 		time_format = re.compile(r'(?P<hour>[12]?[0-9])'
 								r'(:)'
 								r'(?P<min>[0-9][0-9])'
@@ -103,7 +106,6 @@ def parseTrackMeNotTime(trackMeNotTimes):
 		time = time_format.search(entry).group(0)
 		first_colon = time.find(':')
 		second_colon = time.find(':',first_colon+1)
-		print (time)
 		hour = int(time[0:first_colon])
 		
 		minute = int(time[first_colon+1:second_colon])
@@ -156,9 +158,6 @@ def compareQueryTimes(googleTime, trackMeNotTimes):
 			return True
 	return False
 
-		
-
-
 	#return True
 	#check that the googleTime is close enough to at least one of the trackMeNotTimes
 
@@ -182,7 +181,7 @@ def checkTrackMeNotQuery(googleQuery, googleQueryTime, trackMeNotDict):
 			return "No"
 
 
-def createGoogleSearchFile(trackMeNotDict):
+def createGoogleSearchFile(googleActivityFile, googleLogFile, trackMeNotDict):
 	'''
 	parameters:
 		trackMeNotDict: see getTrackMeNotDict()
@@ -193,9 +192,9 @@ def createGoogleSearchFile(trackMeNotDict):
 	(query, time, TrackMeNot)
 	The TrackMeNot column is a "Yes" if the query was made by TrackMeNot. o.w. it is a "No"
 	Note: MyActivity.html file contains the user's entire search history
-	It includes the searches made by TrackmMNot
+	It includes the searches made by TrackMeNot
 	'''
-	f=codecs.open("MyActivity.html", 'r')
+	f=codecs.open(googleActivityFile, 'r')
 
 	try: 
 	    from BeautifulSoup import BeautifulSoup
@@ -223,7 +222,7 @@ def createGoogleSearchFile(trackMeNotDict):
 				r'([ ])'
 				r'(?P<AM_PM>PM)' #   AM|PM)'
 				)
-	with open('GoogleSearchResults.csv', 'w') as csvfile:
+	with open(googleLogFile, 'w') as csvfile:
 		fieldnames = ['Time', 'Query', 'TrackMeNot']
 		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 		writer.writeheader()
@@ -235,14 +234,50 @@ def createGoogleSearchFile(trackMeNotDict):
 				search_for_index = len('Searched for')
 				search_text = wrapper.text[search_for_index+1:time_index]
 				time_text = wrapper.text[time_index:]
-				trackMeNot = checkTrackMeNotQuery(search_text, time_text, trackMeNotDict)
-				writer.writerow({'Time':time_text,'Query':search_text, 'TrackMeNot': trackMeNot})
+				# trackMeNot = checkTrackMeNotQuery(search_text, time_text, trackMeNotDict)
+				# writer.writerow({'Time':time_text,'Query':search_text, 'TrackMeNot': trackMeNot})
 				#print (search_text, time_text)
+				writer.writerow({'Time':time_text,'Query':search_text})
 
-def analyzeGoogleSearches():
+	
+def addTrackMeNotColumn(googleLogFile, trackMeNotDict):
+	with open(googleLogFile,'r') as csvfile:
+		##DON"T READ THE DAMN HEADER
+		reader = csv.reader(csvfile)
+		next(reader) #skip the header
+		trackMeNotColumn = []
+		for record in reader:
+			query_time = record[0]
+			query_text = record[1]
+			trackMeNot = checkTrackMeNotQuery(query_text, query_time, trackMeNotDict)
+			trackMeNotColumn.append(trackMeNot)
+	df = pd.read_csv(googleLogFile)
+	df['TrackMeNot'] = trackMeNotColumn# pd.Series(trackMeNotColumn)
+	df.to_csv(googleLogFile)
+
+
+	# df['TrackMeNot'] = pd.Series(['header', 'data1','data2'])
+	# df.to_csv('GoogleSearchResults.csv', index=False)
+
+
+
+def analyzeGoogleSearches(googleLogFile):
+	'''
+	Determine (guess) which Google search queries were made by TrackMeNot 
+	and determine which Google search queries were authentic
+	Method:
+	for each google search, check if it was made more than once
+	if so, guess that it was
+	First, create log_dict
+		key: query
+		value: amount of times that query was made
+	Then, traverse the dict and assign guesses according to protocol described above
+	'''
 	log_dict = {}
-	with open('GoogleSearchResults.csv', 'r') as f:
+	#create log_dict
+	with open(googleLogFile, 'r') as f:
 		reader = csv.reader(f)
+		next(reader) #SKIP THE HEADER
 		for log in reader:
 			query = log[1]
 			if query in log_dict:
@@ -251,8 +286,9 @@ def analyzeGoogleSearches():
 				log_dict[query] = 1
 	amount_correct = 0
 	amount_wrong  =0
-	with open('GoogleSearchResults.csv', 'r') as f:
+	with open(googleLogFile, 'r+') as f:
 		reader = csv.reader(f)
+		next(reader) #SKIP THE HEADER
 		for log in reader:
 			query = log[1]
 			yes_no = log[2]
@@ -266,21 +302,29 @@ def analyzeGoogleSearches():
 				amount_wrong +=1
 	print ('amount_correct', amount_correct)
 	print ('amount_wrong', amount_wrong)
-	
+
 def main():
+	googleActivityFile = 'MyActivity.html'
+	googleLogFile = 'GoogleSearchResults.csv'
+	trackMeNotLogFile = 'TrackMeNotLogs.csv'
+	trackMeNotDict = getTrackMeNot(trackMeNotLogFile)
+
+	createGoogleSearchFile(googleActivityFile, googleLogFile, trackMeNotDict)
+	addTrackMeNotColumn(googleLogFile, trackMeNotDict)
+	# analyzeGoogleSearches(googleLogFile)
+
+	#addColumn()
+
+
+#print (trackMeNotDict)
+	# month = ['May']
+	# day = [13, 14]
+	# year = [2018]
+
 	# with open('Testing.csv', 'w') as csvfile:
 	# 	fieldnames = ['GoogleTime', 'TrackMeNotTimes']
 	# 	writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 	# 	writer.writeheader()
-	
-	#trackMeNotDict = getTrackMeNot()
-	
-	#print (trackMeNotDict)
-	# month = ['May']
-	# day = [13, 14]
-	# year = [2018]
-	#createGoogleSearchFile(trackMeNotDict)
-	analyzeGoogleSearches()
 
 
 if __name__ == "__main__":
